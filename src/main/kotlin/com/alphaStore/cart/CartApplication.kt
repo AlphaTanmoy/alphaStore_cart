32158523c908391e5cb2a193657970311ff349a2
AlphaTanmoy <69491103+AlphaTanmoy@users.noreply.github.com>
2024-11-20 22:01:33 +0530

    Add files via upload

---
+++ src/main/kotlin/com/alphaStore/cart/CartApplication.kt
@@ -0,0 +1,11 @@
+package com.alphaStore.cart
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CartApplication
+
+fun main(args: Array<String>) {
+	runApplication<CartApplication>(*args)
+}
